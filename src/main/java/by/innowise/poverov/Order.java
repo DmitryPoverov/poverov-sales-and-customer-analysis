bcb075d9764eb7b2762aa80cf0e925e70ba152bc
DP <did.ink@mail.ru>
2025-07-11 11:59:25 +0300

    [Entity classes] - [Corrected] - Added Lombok @annotations for all entities to generate getters, setters, builders, toStrings, equalsAndHashCodes.

--- src/main/java/by/innowise/poverov/Order.java
+++ src/main/java/by/innowise/poverov/Order.java
@@ -1,9 +1,18 @@
 package by.innowise.poverov;
 
+import lombok.*;
+
 import java.time.LocalDateTime;
 import java.util.List;
 
+@Getter
+@Setter
+@Builder
+@ToString
+@EqualsAndHashCode
 public class Order {
+
+    @EqualsAndHashCode.Include
     private String orderId;
     private LocalDateTime orderDate;
     private Customer customer;
--- src/main/java/by/innowise/poverov/OrderItem.java
+++ src/main/java/by/innowise/poverov/OrderItem.java
@@ -1,8 +1,16 @@
 package by.innowise.poverov;
 
+import lombok.*;
+
+@Getter
+@Setter
+@Builder
+@ToString
+@EqualsAndHashCode
 public class OrderItem {
     private String productName;
     private int quantity;
     private double price;
     private Category category;
 }
+
